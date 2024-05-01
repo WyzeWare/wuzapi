c1efd7d37c2c751c20e972b69bdeef3967eae47b
WyzeWare <wyzepalp@gmail.com>
2024-05-01 14:32:40 +0200

    Add support for PostgreSQL database
    - Add a new command-line flag `--dbtype` to specify the database type (sqlite3 or postgres)
    - Add a new command-line flag `--psqlconnstr` to specify the PostgreSQL connection string
    - Add a new command-line flag `--config` to specify the configuration file path for PostgreSQL credentials
    - Implement a `ParseConfigFile` function to parse configuration files
    - Update database connection logic to support both SQLite3 and PostgreSQL databases
    - Update schema creation logic to handle PostgreSQL schema and table creation
    - Add a default PostgreSQL connection string for local development
    - Update logging to provide information about the selected database type and connection details
    
    This commit adds support for using a PostgreSQL database in addition to the existing SQLite3 database.
    The `--dbtype` flag is used to specify the database type, and the `--psqlconnstring` flag is used to provide the
    PostgreSQL connection string. For PostgreSQL, the application can also read the database password from
    a configuration file specified by the `--config` flag.
    
    The database connection and schema creation logic have been updated to handle both SQLite3 and
    PostgreSQL databases. Appropriate logging has been added to provide information about the selected
    database type and connection details.
    
    With this change, the application can now be deployed with either SQLite3 or PostgreSQL as the
    underlying database, allowing more flexibility and scalability as needed.

--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ toolchain go1.22.2
 
 require (
 	github.com/go-resty/resty/v2 v2.11.0
+	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/gorilla/mux v1.8.0
 	github.com/justinas/alice v1.2.0
+	github.com/lib/pq v1.10.9
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/mdp/qrterminal/v3 v3.0.0
 	github.com/patrickmn/go-cache v2.1.0+incompatible
@@ -22,8 +24,11 @@ require (
 require (
 	filippo.io/edwards25519 v1.0.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
@@ -31,11 +36,12 @@ require (
 	github.com/rs/xid v1.5.0 // indirect
 	go.mau.fi/libsignal v0.1.0 // indirect
 	go.mau.fi/util v0.4.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/mod v0.11.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/tools v0.8.0 // indirect
+	golang.org/x/tools v0.10.0 // indirect
 	lukechampine.com/uint128 v1.3.0 // indirect
 	modernc.org/cc/v3 v3.40.0 // indirect
 	modernc.org/ccgo/v3 v3.16.13 // indirect
--- main.go
+++ main.go
@@ -8,17 +8,22 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 	"time"
-	"path/filepath"
-    "go.mau.fi/whatsmeow/store/sqlstore"
-    waLog "go.mau.fi/whatsmeow/util/log"
+
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	waLog "go.mau.fi/whatsmeow/util/log"
+
+	"bufio"
+	"strings"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog"
 	_ "modernc.org/sqlite"
-    _ "github.com/mattn/go-sqlite3"
 )
 
 type server struct {
@@ -28,74 +33,196 @@ type server struct {
 }
 
 var (
-	address    = flag.String("address", "0.0.0.0", "Bind IP Address")
-	port       = flag.String("port", "8080", "Listen Port")
-	waDebug    = flag.String("wadebug", "", "Enable whatsmeow debug (INFO or DEBUG)")
-	logType    = flag.String("logtype", "console", "Type of log output (console or json)")
-	sslcert    = flag.String("sslcertificate", "", "SSL Certificate File")
-	sslprivkey = flag.String("sslprivatekey", "", "SSL Certificate Private Key File")
-	adminToken = flag.String("admintoken", "", "Security Token to authorize admin actions (list/create/remove users)")
-    container *sqlstore.Container
+	address     = flag.String("address", "0.0.0.0", "Bind IP Address")
+	port        = flag.String("port", "8080", "Listen Port")
+	waDebug     = flag.String("wadebug", "", "Enable whatsmeow debug (INFO or DEBUG)")
+	logType     = flag.String("logtype", "console", "Type of log output (console or json)")
+	sslcert     = flag.String("sslcertificate", "", "SSL Certificate File")
+	sslprivkey  = flag.String("sslprivatekey", "", "SSL Certificate Private Key File")
+	adminToken  = flag.String("admintoken", "", "Security Token to authorize admin actions (list/create/remove users)")
+	dbtype      = flag.String("dbtype", "sqlite", "Database type (sqlite or postgres)")
+	psqlConnStr = flag.String("psqlconnstr", "", "Postgres connection string")
+	configFile  = flag.String("config", "", "Path to the configuration file")
+	container   *sqlstore.Container
 
 	killchannel   = make(map[int](chan bool))
 	userinfocache = cache.New(5*time.Minute, 10*time.Minute)
-	log zerolog.Logger
+	log           zerolog.Logger
 )
 
 func init() {
 
 	flag.Parse()
 
-	if(*logType=="json") {
-        log = zerolog.New(os.Stdout).With().Timestamp().Str("role",filepath.Base(os.Args[0])).Logger()
-    } else {
-        output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-        log = zerolog.New(output).With().Timestamp().Str("role",filepath.Base(os.Args[0])).Logger()
-    }
+	if *logType == "json" {
+		log = zerolog.New(os.Stdout).With().Timestamp().Str("role", filepath.Base(os.Args[0])).Logger()
+	} else {
+		output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+		log = zerolog.New(output).With().Timestamp().Str("role", filepath.Base(os.Args[0])).Logger()
+	}
 
-    if(*adminToken == "") {
-        if v := os.Getenv("WUZAPI_ADMIN_TOKEN"); v != "" {
-            *adminToken = v
-        }
-    }
+	if *adminToken == "" {
+		if v := os.Getenv("WUZAPI_ADMIN_TOKEN"); v != "" {
+			*adminToken = v
+		}
+	}
 
+}
+
+// Config represents the parsed configuration data
+type Config map[string]string
+
+// ParseConfigFile parses the configuration file with the specified filename
+func ParseConfigFile(filename string) (Config, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	config := make(Config)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			config[key] = value
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	return config, nil
 }
 
 func main() {
 
-    ex, err := os.Executable()
-    if err != nil {
-        panic(err)
-    }
-    exPath := filepath.Dir(ex)
-
-    dbDirectory := exPath + "/dbdata"
-	_, err = os.Stat(dbDirectory)
-	if os.IsNotExist(err) {
-		errDir := os.MkdirAll(dbDirectory, 0751)
-		if errDir != nil {
-			panic("Could not create dbdata directory")
-		}
-	}
-
-	db, err := sql.Open("sqlite", exPath + "/dbdata/users.db")
+	ex, err := os.Executable()
 	if err != nil {
-		log.Fatal().Err(err).Msg("Could not open/create "+exPath+"/dbdata/users.db")
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+
+	var db *sql.DB
+
+	switch *dbtype {
+	case "sqlite":
+
+		dbDirectory := exPath + "/dbdata"
+		_, err := os.Stat(dbDirectory)
+		if os.IsNotExist(err) {
+			errDir := os.MkdirAll(dbDirectory, 0751)
+			if errDir != nil {
+				panic("Could not create dbdata directory")
+			}
+		}
+
+		db, err = sql.Open("sqlite3", "file:"+exPath+"/dbdata/users.db?_foreign_keys=on")
+		if err != nil {
+			log.Fatal().Err(err).Msg("Could not open/create " + exPath + "/dbdata/users.db")
+			os.Exit(1)
+		}
+		defer db.Close()
+
+		sqlStmt := `CREATE TABLE IF NOT EXISTS users (
+	               id INTEGER NOT NULL PRIMARY KEY, 
+	               name TEXT NOT NULL, 
+	               token TEXT NOT NULL, 
+	               webhook TEXT NOT NULL default "", 
+	               jid TEXT NOT NULL default "", 
+	               qrcode TEXT NOT NULL default "", 
+	               connected INTEGER, 
+	               expiration INTEGER, 
+	               events TEXT NOT NULL default "All"
+	             );`
+		_, err = db.Exec(sqlStmt)
+		if err != nil {
+			panic(fmt.Sprintf("%q: %s\n", err, sqlStmt))
+		}
+
+	case "postgres":
+		if *configFile == "" {
+			panic("Please specify the configuration file with --config")
+		}
+
+		config, err := ParseConfigFile(*configFile)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		// Access configuration values with underscores
+		password := config["whatsapp_postgres_password"]
+
+		// Open PostgreSQL connection
+		if *psqlConnStr == "" {
+			log.Info().Msg("WuzApi will be connected to PostgreSQL at localhost:5432 with default credentials")
+			connectionString := fmt.Sprintf("user=whatsapp password=%s dbname=whatsapp sslmode=disable", password)
+			psqlConnStr = &connectionString
+		} else {
+			log.Info().Msgf("WuzApi will be connected to PostgreSQL at %s", *psqlConnStr)
+			connectionString := fmt.Sprintf("%s password=%s", *psqlConnStr, password)
+			psqlConnStr = &connectionString
+		}
+
+		db, err = sql.Open("postgres", *psqlConnStr)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Could not open PostgreSQL database")
+			os.Exit(1)
+		}
+		defer db.Close()
+
+		// Create table if not exists
+		sqlStmt := `
+        CREATE SCHEMA IF NOT EXISTS whatsapp;
+
+        GRANT USAGE ON SCHEMA whatsapp TO whatsapp;
+        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA whatsapp TO whatsapp;
+        GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA whatsapp TO whatsapp;
+
+        CREATE TABLE IF NOT EXISTS whatsapp.users (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            token TEXT NOT NULL,
+            webhook TEXT DEFAULT '',
+            jid TEXT DEFAULT '',
+            qrcode TEXT DEFAULT '',
+            connected INTEGER,
+            expiration INTEGER,
+            events TEXT DEFAULT 'All'
+		);`
+		_, err = db.Exec(sqlStmt)
+		if err != nil {
+			panic(fmt.Sprintf("%q: %s\n", err, sqlStmt))
+		}
+	default:
+		log.Fatal().Msg("Invalid database type specified")
 		os.Exit(1)
 	}
-	defer db.Close()
 
-	sqlStmt := `CREATE TABLE IF NOT EXISTS users (id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, token TEXT NOT NULL, webhook TEXT NOT NULL default "", jid TEXT NOT NULL default "", qrcode TEXT NOT NULL default "", connected INTEGER, expiration INTEGER, events TEXT NOT NULL default "All");`
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		panic(fmt.Sprintf("%q: %s\n", err, sqlStmt))
-	}
-
-	if(*waDebug!="") {
+	if *waDebug != "" {
 		dbLog := waLog.Stdout("Database", *waDebug, true)
-		container, err = sqlstore.New("sqlite3", "file:"+exPath+"/dbdata/main.db?_foreign_keys=on&_busy_timeout=3000", dbLog)
+		var connectionString string
+		switch *dbtype {
+		case "sqlite":
+			connectionString = "file:" + exPath + "/dbdata/main.db?_foreign_keys=on&_busy_timeout=3000"
+		case "postgres":
+			connectionString = *psqlConnStr
+		}
+		container, err = sqlstore.New(*dbtype, connectionString, dbLog)
 	} else {
-		container, err = sqlstore.New("sqlite3", "file:"+exPath+"/dbdata/main.db?_foreign_keys=on&_busy_timeout=3000", nil)
+		var connectionString string
+		switch *dbtype {
+		case "sqlite":
+			connectionString = "file:" + exPath + "/dbdata/main.db?_foreign_keys=on&_busy_timeout=3000"
+		case "postgres":
+			connectionString = *psqlConnStr
+		}
+		container, err = sqlstore.New(*dbtype, connectionString, nil)
 	}
 	if err != nil {
 		panic(err)
@@ -122,17 +249,17 @@ func main() {
 		if *sslcert != "" {
 			if err := srv.ListenAndServeTLS(*sslcert, *sslprivkey); err != nil && err != http.ErrServerClosed {
 				//log.Fatalf("listen: %s\n", err)
-                log.Fatal().Err(err).Msg("Startup failed")
+				log.Fatal().Err(err).Msg("Startup failed")
 			}
 		} else {
 			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 				//log.Fatalf("listen: %s\n", err)
-                log.Fatal().Err(err).Msg("Startup failed")
+				log.Fatal().Err(err).Msg("Startup failed")
 			}
 		}
 	}()
-    //wlog.Infof("Server Started. Listening on %s:%s", *address, *port)
-    log.Info().Str("address", *address).Str("port",*port).Msg("Server Started")
+	//wlog.Infof("Server Started. Listening on %s:%s", *address, *port)
+	log.Info().Str("address", *address).Str("port", *port).Msg("Server Started")
 
 	<-done
 	log.Info().Msg("Server Stoped")
@@ -144,7 +271,7 @@ func main() {
 	}()
 
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Error().Str("error",fmt.Sprintf("%+v",err)).Msg("Server Shutdown Failed")
+		log.Error().Str("error", fmt.Sprintf("%+v", err)).Msg("Server Shutdown Failed")
 		os.Exit(1)
 	}
 	log.Info().Msg("Server Exited Properly")
